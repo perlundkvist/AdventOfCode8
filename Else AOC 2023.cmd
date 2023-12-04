cfee571a327cb43db4c39fad581f5c3373044180
Per Lundkvist <per.lundkvist@else.se>
2023-12-04 13:09:08 +0100

    Else AOC

--- Else AOC 2023.cmd
+++ Else AOC 2023.cmd
@@ -1,3 +1,4 @@
+@echo off
 C:\Projekt\AdventOfCode8\bin\Debug\net8.0\AdventOfCode8.exe Web 
-;C:\Projekt\AdventOfCode8\bin\Debug\net8.0\AdventOfCode8.exe C:\Projekt\AdventOfCode8\Input\Else.json 
+::C:\Projekt\AdventOfCode8\bin\Debug\net8.0\AdventOfCode8.exe C:\Projekt\AdventOfCode8\Input\Else.json 
 pause
