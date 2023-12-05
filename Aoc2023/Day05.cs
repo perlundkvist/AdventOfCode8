8f39b67e2eae97119a7a51f8f741bc029f296b2e
Per Lundkvist <per.lundkvist@else.se>
2023-12-05 11:11:47 +0100

    Day 5

--- Aoc2023/Day05.cs
+++ Aoc2023/Day05.cs
@@ -11,42 +11,100 @@ namespace AdventOfCode8.Aoc2023
     {
         internal void Run()
         {
-            var input = GetInput("2023_05s");
+            var input = GetInput("2023_05");
 
-            var maps = GetMaps(input);
+            var seeds = GetSeeds(input.First());
+            var maps = GetMaps(input[2..]);
 
-            var lowest = GetLowest(input);
+            var lowest = GetLowest(seeds, maps);
             Console.WriteLine($"Lowest: {lowest}");
 
-            //sum = GetSum2(input);
-            //Console.WriteLine($"Sum2: {sum}");
+            var seeds2 = GetSeeds2(input.First());
+            lowest = GetLowest2(seeds2, maps);
+            Console.WriteLine($"Lowest2: {lowest}");
+        }
+
+        private List<long> GetSeeds(string input)
+        {
+            // seeds: 79 14 55 13
+            var split = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1..];
+            return split.Select(long.Parse).ToList();
+        }
+
+        private List<Tuple<long, long>> GetSeeds2(string input)
+        {
+            // seeds: 79 14 55 13
+            var split = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1..];
+            var pairs = split.Chunk(2).ToList();
+            var seeds = pairs.Select(p => new Tuple<long, long>(long.Parse(p.First()), long.Parse(p.Last()))).ToList();
+            return seeds;
         }
 
         private ImmutableList<Map> GetMaps(List<string> input)
         {
+
+            //seed - to - soil map:
+            //50 98 2
+            //52 50 48
             var maps = new List<Map>();
 
-            foreach (var map in maps)
+            var idx = 0;
+            foreach (var line in input)
             {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    idx++;
+                    continue;
+                }
+                if (!char.IsDigit(line.First()))
+                    continue;
 
+                var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var map = new Map
+                {
+                    Index = idx,
+                    Source = long.Parse(split[1]),
+                    Destination = long.Parse(split[0]),
+                    Length = long.Parse(split[2])
+                };
+                maps.Add(map);
             }
-            
+
             return maps.ToImmutableList<Map>();
         }
 
-        private object GetLowest(List<string> input)
+        private static long GetLowest(List<long> seeds, ImmutableList<Map> maps)
+        {
+            var locations = seeds.Select(s => new { Seed = s, Location = GetLocation(s, maps) }).ToList();
+            return locations.Min(l => l.Location);
+        }
+
+        private long GetLowest2(List<Tuple<long, long>> seeds, ImmutableList<Map> maps)
         {
             throw new NotImplementedException();
+        }
+
+        private static long GetLocation(long seed, ImmutableList<Map> maps)
+        {
+            var value = seed;
+            for (var i = 0; i < 7; i++)
+            {
+                var map = maps.FirstOrDefault(m => m.Index == i && value >= m.Source && value <= m.Source + m.Length);
+                value += map?.Diff ?? 0;
+            }
+
+            return value;
         }
     }
 
     public class Map
     {
-        public enum MapType { Seed2Soil, Soil2Fertilizer, Fertilizer2Water, Water2Light, Light2Temp, Temp2Humidity, Humidity2Location}
-        public MapType Type { get; set; }
+        public int Index{ get; set; }
 
         public long Source { get; set; }
         public long Destination { get; set; }
         public long Length { get; set; }
+        public long Diff => Destination - Source;
+
     }
 }
