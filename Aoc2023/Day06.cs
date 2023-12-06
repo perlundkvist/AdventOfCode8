5a93bee91c70b539a5039c9225612f669ec2e681
Per Lundkvist <per.lundkvist@else.se>
2023-12-06 08:40:21 +0100

    Day 6

--- Aoc2023/Day06.cs
+++ Aoc2023/Day06.cs
@@ -15,22 +15,26 @@ namespace AdventOfCode8.Aoc2023
             var testRaces = new List<Race> { new Race(7, 9), new Race(15, 40), new Race (30, 200)};
             var races = new List<Race> { new Race(56, 334), new Race(71, 1135), new Race(79, 1350), new Race(99, 2430) };
 
-            double time = 56717999;
-            double distance = 334113513502430;
+            long time = 56717999;
+            long distance = 334113513502430;
 
             var margin = GetMargin(races);
             Console.WriteLine($"Margin: {margin}");
 
-            margin = GetMargin2(7, 9);
-            margin = GetMargin2(71530, 940200);
+            margin = GetMargin(new List<Race>{new(time, distance)});
             Console.WriteLine($"Margin: {margin}");
+
+
+            //margin = GetMargin2(7, 9);
+            //margin = GetMargin2(71530, 940200);
+            //Console.WriteLine($"Margin: {margin}");
 
             Console.WriteLine($"{DateTime.Now - start}");
         }
 
         private long GetMargin(List<Race> races)
         {
-            var margin = 1;
+            long margin = 1;
             foreach(var race in races)
             {
                 margin *= GetMargin(race);
@@ -38,10 +42,10 @@ namespace AdventOfCode8.Aoc2023
             return margin;
         }
 
-        private int GetMargin(Race race)
+        private long GetMargin(Race race)
         {
-            var margin = 0;
-            for (int i = 1; i < race.Time;  i++)
+            long margin = 0;
+            for (long i = 1; i < race.Time;  i++)
             {
                 if (GetDistance(i, race.Time) > race.Distance)
                     margin++;
@@ -57,12 +61,12 @@ namespace AdventOfCode8.Aoc2023
             return 1;
         }
 
-        private int GetDistance(int time, int maxTime)
+        private long GetDistance(long time, long maxTime)
         {
             return time * (maxTime - time);
         }
 
 
-        internal record Race(int Time, int Distance);
+        internal record Race(long Time, long Distance);
     }
 }
