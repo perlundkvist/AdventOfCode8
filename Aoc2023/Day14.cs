e086ab916b6c14bc4ce0e1565cd8f5597eb18c89
PerLund <per@lundkvist.com>
2023-12-13 21:46:07 +0100

    Day 14

---
+++ Aoc2023/Day14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode8.Aoc2023
+{
+    internal class Day14 : DayBase
+    {
+        internal void Run()
+        {
+            var start = DateTime.Now;
+
+            var input = GetInput("2023_14s").ToImmutableList();
+            //var records = GetMaps(input);
+
+            //var sum = GetSum(records);
+            //Console.WriteLine($"Sum: {sum}.");
+
+            Console.WriteLine($"{DateTime.Now - start}");
+        }
+    }
+}
