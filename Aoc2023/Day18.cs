873723acd1e059a3c978e025d7f2c426d55bd6ce
PerLund <per@lundkvist.com>
2023-12-17 19:16:39 +0100

    Day 18

---
+++ Aoc2023/Day18.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode8.Aoc2023
+{
+    internal class Day18 : DayBase
+    {
+        internal void Run()
+        {
+            var start = DateTime.Now;
+
+            var input = GetInput("2023_18s");
+
+            //var cost = GetCost(map);
+            //Console.WriteLine($"Cost: {cost}");
+
+            Console.WriteLine($"{DateTime.Now - start}");
+        }
+    }
+}
