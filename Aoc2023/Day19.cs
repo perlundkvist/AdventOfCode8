f72e1a04492718cc9fa5775a5ea5d390f835fcb6
Per Lundkvist <per.lundkvist@else.se>
2023-12-19 13:25:04 +0100

    Day 19

---
+++ Aoc2023/Day19.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode8.Aoc2023
+{
+    internal class Day19 : DayBase
+    {
+        internal void Run()
+        {
+            var start = DateTime.Now;
+
+            Logg.DoLog = false;
+
+            var input = GetInput("2023_19s");
+
+
+
+            Console.WriteLine();
+            Console.WriteLine($"{DateTime.Now - start}");
+        }
+
+        public enum PartType {X, M, A, S}
+
+        public class Part ()
+        {
+
+        }
+
+        private class Workflow
+        {
+            public string Id { get; set; }
+
+            public List<Rule> Rules { get; set; } = new List<Rule>();
+            public string LastResult { get; set; }
+        }
+
+        public class Rule(PartType type, char comparison, int value, string result)
+        {
+
+        }
+    }
+}
