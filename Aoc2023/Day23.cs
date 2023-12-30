fc3aabbece02e8be3ca155d393b00d2a2e0d209a
PerLund <per@lundkvist.com>
2023-12-30 13:47:17 +0100

    Day 23

--- Aoc2023/Day23.cs
+++ Aoc2023/Day23.cs
@@ -21,25 +21,45 @@ namespace AdventOfCode8.Aoc2023
             var map = GetMap(input);
             DrawMap(map);
 
+            //GetLongest(map);
+            map = GetMap2(input);
+            GetLongest(map);
+
+            Console.WriteLine();
+            Console.WriteLine($"{DateTime.Now - start}");
+        }
+
+        private void GetLongest(char[,] map)
+        {
             var startPos = new Position(0, 1);
             var endPos = new Position(map.GetLength(0) - 1, map.GetLength(1) - 2);
             var visited = new HashSet<Position>();
             //DrawMap(map, startPos, visited);
             //DrawMap(map, endPos);
-            Move(startPos, endPos, 0, map, visited);
+
+            //Move(startPos, endPos, 0, map, visited);
+            //Console.WriteLine($"Longest route: {longest}");
+
+            longest = 0;
+            var moves = new Stack<(Position startPos, Position endPos, int steps, HashSet<Position> visited)>();
+
+            moves.Push((startPos, endPos, 0, visited));
+            while (moves.Any())
+            {
+                var move = moves.Pop();
+                var toAdd = Move2(move.startPos, move.endPos, move.steps, map, move.visited);
+                toAdd.ForEach(m => moves.Push(m));
+            }
 
             Console.WriteLine($"Longest route: {longest}");
-
-            Console.WriteLine();
-            Console.WriteLine($"{DateTime.Now - start}");
         }
 
         private void Move(Position startPos, Position endPos, int steps, char[,] map, HashSet<Position> visited)
         {
             if (startPos == endPos) 
             {
-                Console.WriteLine($"Found end: {steps}");
                 longest = Math.Max(steps, longest);
+                Console.WriteLine($"Found end: {steps}. Longest: {longest}");
                 return;
             }
             DrawMap(map, startPos, visited);
@@ -59,33 +79,31 @@ namespace AdventOfCode8.Aoc2023
                 Move(next, endPos, steps + 1, map, [.. visited]);
         }
 
-        private void Move2(Position startPos, Position endPos, int steps, char[,] map, HashSet<Position> visited)
+        private List<(Position startPos, Position endPos, int steps, HashSet<Position> visited)> Move2(Position startPos, Position endPos, int steps, char[,] map, HashSet<Position> visited)
         {
-            var currPos = startPos;
-            while (true)
+            var moves = new List<(Position startPos, Position endPos, int steps, HashSet<Position> visited)>();
+            if (startPos == endPos)
             {
-                if (currPos == endPos)
-                {
-                    Console.WriteLine($"Found end: {steps}");
-                    longest = Math.Max(steps, longest);
-                    continue;
-                }
-                DrawMap(map, currPos, visited);
-                visited.Add(startPos);
-                var (up, down, left, right) = startPos.GetSurrounding(map);
-                var next = right;
-                if (ShouldTry(next, Direction.Right, map, visited))
-                    Move(next, endPos, steps + 1, map, [.. visited]);
-                next = down;
-                if (ShouldTry(next, Direction.Down, map, visited))
-                    Move(next, endPos, steps + 1, map, [.. visited]);
-                next = left;
-                if (ShouldTry(next, Direction.Left, map, visited))
-                    Move(next, endPos, steps + 1, map, [.. visited]);
-                next = up;
-                if (ShouldTry(next, Direction.Up, map, visited))
-                    Move(next, endPos, steps + 1, map, [.. visited]);
+                longest = Math.Max(steps, longest);
+                Console.WriteLine($"Found end: {steps}. Longest: {longest}");
+                return moves;
             }
+            DrawMap(map, startPos, visited);
+            visited.Add(startPos);
+            var (up, down, left, right) = startPos.GetSurrounding(map);
+            var next = right;
+            if (ShouldTry(next, Direction.Right, map, visited))
+                moves.Add(new(next, endPos, steps + 1, [.. visited]));
+            next = down;
+            if (ShouldTry(next, Direction.Down, map, visited))
+                moves.Add(new(next, endPos, steps + 1, [.. visited]));
+            next = left;
+            if (ShouldTry(next, Direction.Left, map, visited))
+                moves.Add(new(next, endPos, steps + 1, [.. visited]));
+            next = up;
+            if (ShouldTry(next, Direction.Up, map, visited))
+                moves.Add(new(next, endPos, steps + 1, [.. visited]));
+            return moves;
         }
 
         private bool ShouldTry(Position? next, Direction direction, char[,] map, HashSet<Position> visited)
@@ -148,6 +166,23 @@ namespace AdventOfCode8.Aoc2023
             for (var l = 0; l < lines; l++)
             {
                 var line = input[l];
+                for (var c = 0; c < cols; c++)
+                {
+                    map[l, c] = line[c];
+                }
+            }
+            return map;
+        }
+
+        private char[,] GetMap2(List<string> input)
+        {
+            var map = new char[input.Count, input.First().Length];
+            var lines = map.GetLength(0);
+            var cols = map.GetLength(1);
+            for (var l = 0; l < lines; l++)
+            {
+                // ^, >, v, and <
+                var line = input[l].Replace('^', '.').Replace('>', '.').Replace('v', '.').Replace('<', '.');
                 for (var c = 0; c < cols; c++)
                 {
                     map[l, c] = line[c];
