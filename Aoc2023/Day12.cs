87eb737962d45177476bab3f03fcd102b021a2ad
PerLund <per@lundkvist.com>
2024-01-05 17:20:03 +0100

    Day 12

--- Aoc2023/Day12.cs
+++ Aoc2023/Day12.cs
@@ -16,8 +16,8 @@ namespace AdventOfCode8.Aoc2023
 
             var start = DateTime.Now;
 
-            var record = new CondictionRecord("?.?????.?????? 1,3,1,3", true);
-            var s = GetSum(record);
+            //var record = new CondictionRecord("?.?????.?????? 1,3,1,3", true);
+            //var s = GetSum(record);
 
             var input = GetInput("2023_12").ToImmutableList();
 
@@ -40,7 +40,7 @@ namespace AdventOfCode8.Aoc2023
             var idx = 1;
             foreach (var record in records)
             {
-                sum += GetSum(record);
+                sum += GetSum2(record);
                 Console.WriteLine($"{idx++} ({records.Count}) {sum}");
             }
             return sum;
@@ -96,6 +96,58 @@ namespace AdventOfCode8.Aoc2023
             Logg.WriteLine();
             return sum;
         }
+
+        private long GetSum2(CondictionRecord record)
+        {
+            var sum = 0L;
+            var springs = new[] { '#', '?' };
+            var rangeList = new List<Path>[record.Corrects.Count];
+            var idx = 0;
+            var field = record.Field;
+            foreach (var correct in record.Corrects)
+            {
+                var paths = new List<Path>();
+                Logg.WriteLine($"{field}: {correct}");
+                var start = 0;
+                var used = rangeList.Count(l => l != null);
+                if (used > 0)
+                {
+                    var last = rangeList[used - 1];
+                    var len = record.Corrects[used - 1];
+                    start = last.First().Range.Start.Value + len + 1;
+                }
+                while (start <= field.Length)
+                {
+                    start = field.IndexOfAny(springs, start);
+                    //Logg.WriteLine($"Start: {start}");
+                    if (start == -1)
+                        break;
+                    var end = start + correct;
+                    if (end > field.Length)
+                        break;
+                    if (IsPossible(start, end, field))
+                        paths.Add(new Path(new Range(start, end)));
+                    start++;
+                }
+                Logg.WriteLine($"Paths: {string.Join(",", paths)}");
+                rangeList[idx++] = paths;
+            }
+            //var combinations = GetCombinations(rangeList);
+            //sum += TestCombinations(combinations, field);
+            //Logg.WriteLine($"{combinations.Count} combinations, {sum} valid");
+            rangeList[0] = rangeList[0].Where(r => IsPossible(new Range(0, 0), r.Range, field)).ToList();
+            rangeList[record.Corrects.Count - 1] = rangeList[record.Corrects.Count - 1].Where(r => IsPossible(r.Range, new Range(999, 999), field)).ToList();
+            for (int i = 1; i < record.Corrects.Count - 1; i++)
+            {
+                var after = rangeList[i + 1].Last();
+                rangeList[i] = rangeList[i].Where(r => r.Range.End.Value < after.Range.Start.Value).ToList();
+            }
+            sum += GetCombinations3(rangeList, field, 0);
+            Logg.WriteLine($"Sum: {sum}");
+            Logg.WriteLine();
+            return sum;
+        }
+
 
         [GeneratedRegex("#")]
         private static partial Regex HashRegex();
@@ -180,6 +232,37 @@ namespace AdventOfCode8.Aoc2023
             return sum;
         }
 
+        private long GetCombinations3(List<Path>[] pathList, string field, int depth)
+        {
+            if (pathList.Length == 1)
+            {
+                pathList[0].ForEach(p => p.Steps = 1);
+                return pathList[0].Count;
+            }
+            var sum = 0L;
+            foreach (var path in pathList[0])
+            {
+                if (path.Steps.HasValue)
+                {
+                    sum += path.Steps.Value;
+                    continue;
+                }
+                var greater = pathList[1].Where(r => r.Range.Start.Value > path.Range.End.Value).ToList();
+                greater = greater.Where(g => IsPossible(path.Range, g.Range, field)).ToList();
+                if (greater.Count == 0)
+                    continue;
+                //Logg.Write($"{depth}; {range},");
+                var newList = new List<Path>[pathList.Length - 1];
+                newList[0] = greater;
+                if (pathList.Length > 2)
+                    Array.Copy(pathList, 2, newList, 1, newList.Length - 1);
+                path.Steps = GetCombinations3(newList, field, depth + 1);
+                sum += path.Steps.Value;
+            }
+            return sum;
+        }
+
+
         private bool IsPossible(Range start, Range end, string field)
         {
             var indexes  = HashRegex().Matches(field).ToList().Select(m => m.Index).ToList();
@@ -225,6 +308,17 @@ namespace AdventOfCode8.Aoc2023
                     Corrects.AddRange(split[1].Split(",").Select(i => int.Parse(i)).ToList());
                 }
                 Groups = Field.Split('.', StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+        }
+
+        public class Path(Range range)
+        {
+            public Range Range { get; } = range;
+            public long? Steps { get; set; } = null;
+
+            public override string ToString()
+            {
+                return $"{Range}:{Steps}";
             }
         }
 
