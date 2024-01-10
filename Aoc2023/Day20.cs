7ce4612cb72ffcb20c34db00f32e09c544863607
Per Lundkvist <per.lundkvist@else.se>
2024-01-10 16:05:47 +0100

    Day 20

--- Aoc2023/Day20.cs
+++ Aoc2023/Day20.cs
@@ -16,37 +16,167 @@ namespace AdventOfCode8.Aoc2023
 
             var input = GetInput("2023_20s");
 
+            var modules = input.Select(Module.Create).ToList();
 
+            var sum = PressButton(modules);
+            Console.WriteLine($"Total: {sum}");
 
             Console.WriteLine();
             Console.WriteLine($"{DateTime.Now - start}");
         }
 
+        private object PressButton(List<Module> modules)
+        {
+            var timesPressed = 0;
+            var lowSent = 0;
+            var highSent = 0;
+            while (true)
+            {
+                if (timesPressed > 4000)
+                    break;
+                if (timesPressed > 0 && modules.All(m => !m.GetState()))
+                {
+                    break;
+                }
+                var clone = modules.Select(m => (Module)m.Clone()).ToList();
+                var broadcaster = clone.First(m => m.Name == "broadcaster");
+                broadcaster.HandlePulse(true, "broadcaster");
+                foreach (var module in modules)
+                {
+                    var (lowPulses, highPulses) = module.SendPulses(clone);
+                    lowSent += lowPulses;
+                    highSent += highPulses;
+                }
+                modules = clone;
+                timesPressed++;
+            }
+            return lowSent * highSent;
+        }
+
         public abstract class Module(string name)
         {
             public string Name { get; } = name;
-            public abstract void HandlePulse(bool state, List<Module> modules);
+            protected readonly List<string> Receivers = new();
+
+            public static Module Create(string config) 
+            {
+                var split = config.Split(" -> ");
+                var type = split[0][0];
+                Module module = type switch
+                {
+                    '%' => new FlipFlop(split[0][1..]),
+                    '&' => new Conjunction(split[0][1..]),
+                    _ => new Broadcaster(split[0])
+                };
+                module.Receivers.AddRange(split[1].Split(", ").ToList());
+                return module;
+            }
+
+            public abstract bool GetState();
+            public abstract void HandlePulse(bool state, string sender);
+            public abstract (int lowPulses, int highPulses) SendPulses(List<Module> modules);
+            public abstract object Clone();
         }
 
         public class FlipFlop(string name) : Module(name)
         {
-            public bool State{ get; private set; } = false;
+            private bool _state;
+            private bool _sendPulse;
 
-            public override void HandlePulse(bool state, List<Module> modules)
+            public override bool GetState()
             {
-                throw new NotImplementedException();
+                return _state;
             }
-        }
 
-        public class Broadcaster(string name, List<string> receivers) : Module(name)
-        {
-            public override void HandlePulse(bool state, List<Module> modules)
+            public override void HandlePulse(bool state, string sender)
             {
-                foreach (var receiver in receivers)
+                _sendPulse = !state;
+                if (state)
+                    return;
+                _state = !_state;
+            }
+
+            public override (int lowPulses, int highPulses) SendPulses(List<Module> modules)
+            {
+                if (!_sendPulse)
+                    return (0, 0);
+                var lowPulses = !_state ? Receivers.Count : 0;
+                var highCount = _state ? Receivers.Count : 0;
+                foreach (var receiver in Receivers)
                 {
-                    modules.First(m => m.Name == receiver).HandlePulse(state, modules);
+                    modules.First(m => m.Name == receiver).HandlePulse(_state, name);
                 }
+                return (lowPulses, highCount);
+            }
+            public override object Clone()
+            {
+                return new FlipFlop(Name) { _state = _state };
             }
         }
+
+        public class Conjunction(string name) : Module(name)
+        {
+            private readonly Dictionary<string, bool> _states = new();
+
+            public override bool GetState()
+            {
+                return _states.Values.All(v => v);
+            }
+
+            public override void HandlePulse(bool state, string sender)
+            {
+                if (!_states.TryAdd(sender, state))
+                    _states[sender] = state;
+            }
+
+            public override (int lowPulses, int highPulses) SendPulses(List<Module> modules)
+            {
+                var state = _states.Values.Any(v => !v);
+                var lowPulses = !state ? Receivers.Count : 0;
+                var highCount = state ? Receivers.Count : 0;
+                foreach (var receiver in Receivers)
+                {
+                    modules.First(m => m.Name == receiver).HandlePulse(state, name);
+                }
+                return (lowPulses, highCount);
+            }
+            public override object Clone()
+            {
+                var conjunction = new Conjunction(Name);
+                foreach (var state in _states)
+                    conjunction._states.TryAdd(state.Key, state.Value);
+                return conjunction;
+            }
+        }
+
+        public class Broadcaster(string name) : Module(name)
+        {
+            private bool _state;
+            public override bool GetState()
+            {
+                return _state;
+            }
+
+            public override void HandlePulse(bool state, string sender)
+            {
+                _state = state;
+            }
+
+            public override (int lowPulses, int highPulses) SendPulses(List<Module> modules)
+            {
+                foreach (var receiver in Receivers)
+                {
+                    modules.First(m => m.Name == receiver).HandlePulse(_state, name);
+                }
+                var lowPulses = !_state ? Receivers.Count : 0;
+                var highCount = _state ? Receivers.Count : 0;
+                return (lowPulses, highCount);
+            }
+            public override object Clone()
+            {
+                return new Broadcaster(Name) {_state = _state};
+            }
+        }
+
     }
 }
