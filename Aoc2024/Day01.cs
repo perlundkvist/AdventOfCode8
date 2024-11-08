7a464bfd2ba7f3ce0a85495da09b23c447515ca2
perlundkvist <per.lundkvist@else.se>
2024-11-08 10:30:54 +0100

    2024

---
+++ Aoc2024/Day01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdventOfCode8.Aoc2024
+{
+    internal class Day01 : DayBase
+    {
+        internal void Run()
+        {
+            var input = GetInput("2024_01");
+
+            //Console.WriteLine($"Sum: {sum}");
+
+        }
+
+    }
+}
