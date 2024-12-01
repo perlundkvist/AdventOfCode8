9b8618fc6c805a788c222601e9521041794ff2af
PerLund <per@lundkvist.com>
2024-12-01 06:44:42 +0100

    2024 Day 1

--- Aoc2024/Day01.cs
+++ Aoc2024/Day01.cs
@@ -15,7 +15,29 @@ namespace AdventOfCode8.Aoc2024
         {
             var input = GetInput("2024_01");
 
-            //Console.WriteLine($"Sum: {sum}");
+            var numbers1 = new List<int>();
+            var numbers2 = new List<int>();
+
+            foreach (var line in input)
+            {
+                var numbers = line.Split("   ").Select(int.Parse).ToList();
+                numbers1.Add(numbers[0]);
+                numbers2.Add(numbers[1]);
+            }
+
+            numbers1.Sort();
+            numbers2.Sort();
+
+            var distance = 0;
+            var similarity = 0;
+            for (var i = 0; i < numbers1.Count; i++)
+            {
+                distance += Math.Abs(numbers1[i] - numbers2[i]);
+                similarity += numbers1[i] * numbers2.Count(n => n == numbers1[i]);
+            }
+
+            Console.WriteLine($"Distance: {distance}");
+            Console.WriteLine($"Similarity: {similarity}");
 
         }
 
