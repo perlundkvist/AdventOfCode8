1382b9cc41f11dfdb502d9376df92c292cba5a92
PerLund <per@lundkvist.com>
2024-12-06 13:05:57 +0100

    2024 Day 6

--- Aoc2024/Day06.cs
+++ Aoc2024/Day06.cs
@@ -25,13 +25,20 @@ internal class Day06 : DayBase
 
         Console.WriteLine($"Visited: {visited.Count}");
 
+        //obstacles.Add(new Position(6, 3));
+        //Console.WriteLine($"{Move2(obstacles, Direction.Up, current, input.Count, input[0].Length)}");
+        //Console.WriteLine($"{Move3(obstacles, Direction.Up, current, input.Count, input[0].Length)}");
+        //obstacles.Remove(new Position(6, 3));
+
         var newObstructions = 0;
         var count = 0;
-        foreach (var obstacle in visited.ToList())
+        foreach (var obstacle in visited)
         {
             Console.WriteLine($"{count++}");
             obstacles.Add(obstacle); 
-            if (!Move2(obstacles, Direction.Up, current, input.Count, input[0].Length))
+            //if (!Move2(obstacles, Direction.Up, current, input.Count, input[0].Length))
+            //    newObstructions++;
+            if (!Move3(obstacles, Direction.Up, current, input.Count, input[0].Length))
                 newObstructions++;
             obstacles.Remove(obstacle);
         }
@@ -137,6 +144,68 @@ internal class Day06 : DayBase
                         return true;
                     if (!obstacles.Contains(new Position(nextLine, nextCol)))
                         break;
+                    nextCol--;
+                    direction = Direction.Down;
+                    break;
+            }
+
+            current = new Position(nextLine, nextCol);
+        }
+    }
+
+    private bool Move3(List<Position> obstacles, Direction direction, Position current, int lines, int cols)
+    {
+        List<Position<Direction>> visitedDirections = [];
+        while (true)
+        {
+            if (visitedDirections.Contains(new Position<Direction>(current.Line, current.Col, direction)))
+                return false;
+            visitedDirections.Add(new Position<Direction>(current.Line, current.Col, direction));
+            var nextCol = current.Col;
+            var nextLine = current.Line;
+            switch (direction)
+            {
+                case Direction.Up:
+                    var above = obstacles.OrderBy(o => o.Line).LastOrDefault(o => o.Line < nextLine && o.Col == nextCol);
+                    if (above == null)
+                        return true;
+                    for (; nextLine > above.Line; nextLine--)
+                    {
+                        visitedDirections.Add(new Position<Direction>(nextLine, nextCol, direction));
+                    }
+                    nextLine++;
+                    direction = Direction.Right;
+                    break;
+                case Direction.Down:
+                    var below = obstacles.OrderBy(o => o.Line).FirstOrDefault(o => o.Line > nextLine && o.Col == nextCol);
+                    if (below == null)
+                        return true;
+                    for (; nextLine < below.Line; nextLine++)
+                    {
+                        visitedDirections.Add(new Position<Direction>(nextLine, nextCol, direction));
+                    }
+                    nextLine--;
+                    direction = Direction.Left;
+                    break;
+                case Direction.Left:
+                    var left = obstacles.OrderBy(o => o.Col).LastOrDefault(o => o.Line == nextLine && o.Col < nextCol);
+                    if (left == null)
+                        return true;
+                    for (; nextCol > left.Col; nextCol--)
+                    {
+                        visitedDirections.Add(new Position<Direction>(nextLine, nextCol, direction));
+                    }
+                    nextCol++;
+                    direction = Direction.Up;
+                    break;
+                case Direction.Right:
+                    var right = obstacles.OrderBy(o => o.Col).FirstOrDefault(o => o.Line == nextLine && o.Col > nextCol);
+                    if (right == null)
+                        return true;
+                    for (; nextCol < right.Col; nextCol++)
+                    {
+                        visitedDirections.Add(new Position<Direction>(nextLine, nextCol, direction));
+                    }
                     nextCol--;
                     direction = Direction.Down;
                     break;
