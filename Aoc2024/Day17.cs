82012a6dbc4d46cc60a9ca95c968783bd7e4a141
PerLund <per@lundkvist.com>
2024-12-17 21:11:20 +0100

    2024 Day 17

--- Aoc2024/Day17.cs
+++ Aoc2024/Day17.cs
@@ -16,40 +16,117 @@ internal class Day17 : DayBase
     {
         Logg.DoLog = true;
 
-        Execute([0, 1], 0);
+        Registers[0] = 0;
+        Registers[1] = 0;
+        Registers[2] = 9;
+        Execute([2, 6]);
 
-        const bool example = true;
-        Registers[0] = example ? 729 : 30886132;
-        //Registers[1] = example ? 5 : 0;
+        Registers[0] = 10;
+        Registers[1] = 0;
+        Registers[2] = 0;
+        Execute([5, 0, 5, 1, 5, 4]);
 
-        List<int> program = example
-            ? [0, 1, 5, 4, 3, 0]
-            : [2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0];
+        Registers[0] = 2024;
+        Registers[1] = 0;
+        Registers[2] = 0;
+        Execute([0, 1, 5, 4, 3, 0]);
+
+        Registers[0] = 0;
+        Registers[1] = 29;
+        Registers[2] = 0;
+        Execute([1, 7]);
+
+        Registers[0] = 0;
+        Registers[1] = 2024;
+        Registers[2] = 43690;
+        Execute([4, 0]);
+
+        Registers[0] = 729;
+        Registers[1] = 0;
+        Registers[2] = 0;
+        Execute([0, 1, 5, 4, 3, 0]);
 
         #region Part 1
 
-        var idx = 0;
-        while (idx >= 0 && idx < program.Count)
-        {
-            idx = Execute(program, idx);
-        }
+        Registers[0] = 30886132;
+        Registers[1] = 0;
+        Registers[2] = 0;
+        Execute([2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0]);
 
+        #endregion
 
-        Console.WriteLine($"Output: {string.Join(',', Output)}");
+        #region Part 2
+
+        Registers[0] = 117440;
+        Registers[1] = 0;
+        Registers[2] = 0;
+        Execute([0, 3, 5, 4, 3, 0]);
+        Execute2([0, 3, 5, 4, 3, 0]);
+
+        Execute2([2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0]);
+
 
         #endregion
 
     }
 
-    private int Execute(List<int> program, int idx)
+    private void Execute2(List<int> program)
+    {
+        Output.Clear();
+        for (var i = 0; i < int.MaxValue; i++)
+        {
+            if (i % 1000000 == 0)
+                Logg.WriteLine($"{i}/{int.MaxValue}");
+            Output.Clear();
+            Registers[0] = i;
+            Registers[1] = 0;
+            Registers[2] = 0;
+            try
+            {
+                var idx = 0;
+                while (idx >= 0 && idx < program.Count)
+                {
+                    idx = Execute(program, idx, true);
+                }
+
+                //Logg.WriteLine($"Program: {string.Join(',', program)}");
+                //Logg.WriteLine($"Output:  {string.Join(',', Output)}");
+                if (Output.SequenceEqual(program))
+                {
+                    Console.WriteLine($"Found: {i}");
+                    break;
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+
+    private void Execute(List<int> program)
+    {
+        Output.Clear();
+        Logg.WriteLine($"Registers {Registers[0]},{Registers[1]},{Registers[2]}");
+        var idx = 0;
+        while (idx >= 0 && idx < program.Count)
+        {
+            idx = Execute(program, idx);
+        }
+        Logg.WriteLine($"Registers {Registers[0]},{Registers[1]},{Registers[2]}");
+        Logg.WriteLine($"Output: {string.Join(',', Output)}");
+        Logg.WriteLine();
+    }
+
+    private int Execute(List<int> program, int idx, bool compare = false)
     {
         var opcode = program[idx];
         var literal = program[idx + 1];
-        var combo = literal <= 3 ? literal : Registers[literal - 4];
+        var combo = literal is <= 3 or 7 ? literal : Registers[literal - 4];
         switch (opcode)
         {
             case 0: // adv
-                Registers[0] /= (int) Math.Pow(2, combo);
+                Registers[0] /= (int)Math.Pow(2, combo);
                 return idx + 2;
             case 1: // bxl
                 Registers[1] ^= literal;
@@ -65,12 +142,16 @@ internal class Day17 : DayBase
                 return idx + 2;
             case 5: // out
                 Output.Add(combo % 8);
+                if (compare && Output.Count > program.Count)
+                    return 9999;
+                if (compare && !Output.SequenceEqual(program[..Output.Count]))
+                    return 9999;
                 return idx + 2;
             case 6: // bdv
-                Registers[1] /= (int)Math.Pow(2, combo);
+                Registers[1] = Registers[0] / (int)Math.Pow(2, combo);
                 return idx + 2;
             case 7: // cdv
-                Registers[2] /= (int)Math.Pow(2, combo);
+                Registers[2] = Registers[0] / (int)Math.Pow(2, combo);
                 return idx + 2;
         }
 
