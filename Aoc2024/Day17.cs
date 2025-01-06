37548541daa4cc5cdef0dc22f0d2bfd21474ed61
PerLund <per@lundkvist.com>
2025-01-06 14:19:05 +0100

    2024 Day 17

--- Aoc2024/Day17.cs
+++ Aoc2024/Day17.cs
@@ -14,7 +14,7 @@ internal class Day17 : DayBase
 
     internal void Run()
     {
-        Logg.DoLog = true;
+        Logg.DoLog = false;
 
         //Registers[0] = 0;
         //Registers[1] = 0;
@@ -57,12 +57,18 @@ internal class Day17 : DayBase
 
         #region Part 2
 
-        //Registers[0] = 117440;
-        //Registers[1] = 0;
-        //Registers[2] = 0;
-        //Execute([0, 3, 5, 4, 3, 0]);
-        //Execute2([0, 3, 5, 4, 3, 0]);
+        Logg.DoLog = true;
+        Registers[0] = Convert.ToInt64("56".PadRight(16, '0'), 8);
+        Registers[1] = 0;
+        Registers[2] = 0;
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    Registers[0] = i;
+        //    Execute([2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0]);
+        //}
+        //Execute([2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0]);
 
+        Logg.DoLog = false;
         Execute2([2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 4, 5, 5, 3, 0]);
 
 
@@ -73,17 +79,23 @@ internal class Day17 : DayBase
     private void Execute2(List<int> program)
     {
         Output.Clear();
-        var lastFound = 0L;
         var numFound = 0;
-        var numTest = 6;
-        for (long i = 14690858; i < long.MaxValue; i++)
+        var numTest = 20;
+        var digits = 2;
+        var start = Convert.ToInt64("5611532756".PadRight(digits, '0'), 8);
+        var end =   Convert.ToInt64("5611532757".PadRight(digits, '0'), 8);
+        for (long i = start; i < long.MaxValue; i++)
         {
-            if (numFound == 100)
+            if (i > end)
             {
-                break;
+                digits++;
+                start = Convert.ToInt64("5611532756".PadRight(digits, '0'), 8);
+                end =   Convert.ToInt64("5611532757".PadRight(digits, '0'), 8);
+                i = start;
             }
-            //if (i % 1000000 == 0)
-            //    Logg.WriteLine($"{i}/{int.MaxValue}");
+
+            if (i % 10000000 == 0)
+                Logg.WriteLine($"{i}/{int.MaxValue}");
             Output.Clear();
             Registers[0] = i;
             Registers[1] = 0;
@@ -98,29 +110,20 @@ internal class Day17 : DayBase
 
                 //Logg.WriteLine($"Program: {string.Join(',', program)}");
                 //Logg.WriteLine($"Output:  {string.Join(',', Output)}");
-                if (Output.Count >= numTest)
-                {
-                    var found = true;
-                    for (var j = 0; j < numTest; j++)
-                    {
-                        if (Output[j] == program[j]) 
-                            continue;
-                        found = false;
-                        break;
-                    }
+                if (Output.Count > numTest)
+                    break;
 
-                    if (found)
-                    {
-                        Console.WriteLine($"Found: {i} {Convert.ToString(i, 16).PadLeft(8, '0')} Diff: {i - lastFound}");
-                        lastFound = i;
-                        numFound++;
-                    }
+                var last = program[^Output.Count..];
+                if (Output.SequenceEqual(last))
+                {
+                    Console.WriteLine($"Found: {Convert.ToString(i, 8).PadLeft(8, '0')} Output: {string.Join(',', Output)}");
+                    numFound++;
                 }
-                //if (Output.SequenceEqual(program))
-                //{
-                //    Console.WriteLine($"Found: {i}");
-                //    break;
-                //}
+                if (Output.SequenceEqual(program))
+                {
+                    Console.WriteLine($"Found: {i}");
+                    break;
+                }
             }
             catch
             {
@@ -148,24 +151,30 @@ internal class Day17 : DayBase
         var opcode = program[idx];
         var literal = program[idx + 1];
         long combo = literal is <= 3 or 7 ? literal : Registers[literal - 4];
+        var comboDesc = literal is <= 3 or 7 ? literal.ToString() : $"{(char) ('A' + literal - 4)}";
         switch (opcode)
         {
             case 0: // adv
+                Logg.WriteLine($"A = A / 2^{comboDesc} Value: ({combo})");
                 Registers[0] /= (int)Math.Pow(2, combo);
                 return idx + 2;
             case 1: // bxl
+                Logg.WriteLine($"B = B XOR {literal}");
                 Registers[1] ^= literal;
                 return idx + 2;
             case 2: // bst
+                Logg.WriteLine($"B = {comboDesc} % 8  Value: ({combo})");
                 Registers[1] = combo % 8;
                 return idx + 2;
             case 3: // jnz
-                Registers[1] = combo % 8;
+                Logg.WriteLine($"JMP IF A != 0. A = {Registers[0]}. JMP {literal}");
                 return Registers[0] == 0 ? idx + 2 : literal;
             case 4: // bxc
+                Logg.WriteLine("B = B XOR C");
                 Registers[1] ^= Registers[2];
                 return idx + 2;
             case 5: // out
+                Logg.WriteLine($"Output {comboDesc} % 8 Value: ({combo})");
                 Output.Add((int)(combo % 8));
                 if (Output.Count > breakAt)
                 {
@@ -174,9 +183,11 @@ internal class Day17 : DayBase
 
                 return idx + 2;
             case 6: // bdv
+                Logg.WriteLine($"B = A / 2^{comboDesc} Value: ({combo})");
                 Registers[1] = Registers[0] / (int)Math.Pow(2, combo);
                 return idx + 2;
             case 7: // cdv
+                Logg.WriteLine($"C = A / 2^{comboDesc} Value: ({combo})");
                 Registers[2] = Registers[0] / (int)Math.Pow(2, combo);
                 return idx + 2;
         }
